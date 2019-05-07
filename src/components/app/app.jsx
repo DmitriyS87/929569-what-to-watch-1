54ea27659444d1c2930ec657163ccac323e082d8
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-08 02:49:53 +0800

    trows data props to App with PropTypes checking

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import MainPage from '../main/main-page.jsx';
 
-const App = () => {
+
+const App = (props) => {
+  const {data} = props;
+
   return (
-    <MainPage />
+    <MainPage movies={data.movies.names} />
   );
+};
+
+App.propTypes = {
+  data: PropTypes.object.isRequired
 };
 
 export default App;
