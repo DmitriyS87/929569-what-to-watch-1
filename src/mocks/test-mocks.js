c7dcc4b32a564b22e71aa2e0f5be3f9e41c180b4
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-19 21:50:39 +0800

    makes TEST_MOCKS for all tests

---
+++ src/mocks/test-mocks.js
@@ -0,0 +1,12 @@
+export default {
+  films: [{
+    title: `Aviator`,
+    coverSrc: `img/aviator.jpg`,
+    id: 0
+  }],
+  film: {
+    title: `Aviator`,
+    coverSrc: `img/aviator.jpg`,
+    id: 0
+  },
+};
