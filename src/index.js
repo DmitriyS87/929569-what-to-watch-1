e0c09bf09a79c921d6d9c81e77dc3bcc124770b8
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-19 21:54:27 +0800

    deletes usless MOVIES

--- src/index.js
+++ src/index.js
@@ -7,29 +7,6 @@ import films from './mocks/films.js';
 const entryPoint = document.getElementById(`root`);
 
 const init = () => {
-  // const MOVIES = [
-  //   `Fantastic Beasts: The Crimes of Grindelwald`,
-  //   `Bohemian Rhapsody`,
-  //   `Macbeth`,
-  //   `Aviator`,
-  //   `We need to talk about Kevin`,
-  //   `What We Do in the Shadows`,
-  //   `Revenant`,
-  //   `Johnny English`,
-  //   `Shutter Island`,
-  //   `Pulp Fiction`,
-  //   `No Country for Old Men`,
-  //   `Snatch`,
-  //   `Moonrise Kingdom`,
-  //   `Seven Years in Tibet`,
-  //   `Midnight Special`,
-  //   `War of the Worlds`,
-  //   `Dardjeeling Limited`,
-  //   `Orlando`,
-  //   `Mindhunter`,
-  //   `Midnight Special`
-  // ];
-
   ReactDOM.render(<App movies={films} />, entryPoint);
 };
 
