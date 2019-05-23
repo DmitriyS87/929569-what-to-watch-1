c9639b2782f7d254534b3fe3403f5d1b0a9b9a69
DmitriyS87 <schukindmitriy@gmail.com>
2019-05-23 18:40:48 +0800

    adds href: for each  mocks/films

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -2,26 +2,31 @@ export default [
   {
     title: `Aviator`,
     coverSrc: `img/aviator.jpg`,
-    id: 0
+    id: 0,
+    herf: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
   },
   {
     title: `Pulp Fiction`,
     coverSrc: `img/pulp-fiction.jpg`,
-    id: 1
+    id: 1,
+    herf: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     title: `Mindhunter`,
     coverSrc: `img/mindhunter.jpg`,
-    id: 2
+    id: 2,
+    herf: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv`,
   },
   {
     title: `Shutter Island`,
     coverSrc: `img/shutter-island.jpg`,
-    id: 3
+    id: 3,
+    herf: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
   },
   {
     title: `What We Do in the Shadows`,
     coverSrc: `img/what-we-do-in-the-shadows.jpg`,
-    id: 4
+    id: 4,
+    herf: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`,
   }
 ];
