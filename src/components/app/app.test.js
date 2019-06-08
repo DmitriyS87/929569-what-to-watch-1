dc13e5c9c5553a96a8725e3a57091f26121383e7
DmitriyS87 <schukindmitriy@gmail.com>
2019-06-08 17:08:02 +0800

    updates components and reducers tests after new SignIn and new user state

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,16 +1,58 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {App} from '../app/app.jsx';
-import TEST_MOCKS from '../../mocks/test-mocks.js';
 
-it(`App correctly renders after relunch`, () => {
+const mock = {
+  active: `All genres`,
+  movies: [{
+    title: `Aviator`,
+    coverSrc: `img/aviator.jpg`,
+    id: 0,
+    links: [
+      {
+        href: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
+        format: `webm`,
+      },
+      {
+        href: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv`,
+        format: `ogv`,
+      },
+    ],
+    genre: `Adventure`,
+  }],
+};
+
+
+it(`App correctly renders MainPage without user logon`, () => {
   const mockFunction = jest.fn();
   const tree = renderer
-  .create(<App
-    setGenre={mockFunction}
-    {...TEST_MOCKS.dispatchToProps}
-  />)
-  .toJSON();
+    .create(<App
+      {...mock}
+      isAuthorizationRequired={false}
+      setGenre={mockFunction}
+      tryLogin={mockFunction}
+      checkUser={mockFunction}
+      errorMessage={``}
+      user={null}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+it(`App correctly renders MainPage with user logon`, () => {
+  const mockFunction = jest.fn();
+  const tree = renderer
+    .create(<App
+      {...mock}
+      isAuthorizationRequired={false}
+      setGenre={mockFunction}
+      tryLogin={mockFunction}
+      checkUser={mockFunction}
+      errorMessage={``}
+      user={{id: `mock`}}
+    />)
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
