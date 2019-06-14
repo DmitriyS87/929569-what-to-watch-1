8e5bd0e8ce57f3da36889cafbce76f3cf791d187
DmitriyS87 <schukindmitriy@gmail.com>
2019-06-14 22:48:31 +0800

    changes selector getAdaptedMovies to use all properties

--- src/reducers/data/selectors.js
+++ src/reducers/data/selectors.js
@@ -12,16 +12,44 @@ export const getAdaptedMovies = (state) => {
     return data.map((movie) => {
       return {
         title: movie.name,
-        coverSrc: movie[`preview_image`],
+        poster: {
+          src: movie[`poster_image`],
+          alt: ``
+        },
+        previewImg: {
+          src: movie[`preview_image`],
+          alt: ``
+        },
         id: movie.id,
-        links: [{
+        previewMovie: [{
           href: movie[`preview_video_link`],
           format: `mp4`
         }],
         genre: movie.genre,
+        director: movie.director,
+        description: movie.description,
+        releseYear: movie.released,
+        runTime: movie[`run_time`],
+        rating: movie.rating,
+        isFavorite: movie[`is_favorite`],
+        scoresCount: movie[`scores_count`],
+        starring: movie.starring,
+        fullMovie: {
+          href: movie[`video_link`],
+        },
+        backgroundColor: movie[`background_color`],
+        backgroundImg: movie[`background_image`],
       };
     });
   };
 
   return transformToAppData(serverFormatMovies);
+};
+
+export const getMovie = (state, id) => {
+  const movie = state[NAME_SPACE].movies.find((it) => it.id === id);
+  if (movie) {
+    return movie;
+  }
+  return {};
 };
