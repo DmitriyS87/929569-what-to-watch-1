9a60e4f4a861c96e0f23b68c3718d26cbddf4e9c
DmitriyS87 <schukindmitriy@gmail.com>
2019-06-14 22:55:15 +0800

    updates tests

--- src/components/small-movie-card/small-movie-card.e2e.test.tsx
+++ src/components/small-movie-card/small-movie-card.e2e.test.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as Adapter from 'enzyme-adapter-react-16';
 import { configure, shallow } from 'enzyme';
-import SmallMovieCard from './small-movie-card';
+import { SmallMovieCard } from './small-movie-card';
 import TEST_MOCKS from '../../mocks/test-mocks.js';
 
 configure({ adapter: new Adapter() });
@@ -9,6 +9,10 @@ configure({ adapter: new Adapter() });
 const MOCK_ACTIVE = false;
 
 jest.useFakeTimers();
+const mockHistory = { push: jest.fn() };
+const mockEvt = {
+  preventDefault() { }
+}
 
 it(`SmallMovieCard return for calback movie.id on mouseenter`, () => {
   const mockFunctionFocus = jest.fn();
@@ -18,6 +22,7 @@ it(`SmallMovieCard return for calback movie.id on mouseenter`, () => {
     onFocus={mockFunctionFocus}
     onBlur={mockHandler}
     active={MOCK_ACTIVE}
+    history={mockHistory}
   />);
   expect(mockFunctionFocus).not.toBeCalled();
   const movieCard = smallMovieCard.find(`.small-movie-card`);
@@ -35,10 +40,27 @@ it(`SmallMovieCard correctly calles callback onMouseLeave`, () => {
     onFocus={mockFunctionFocus}
     onBlur={mockHandler}
     active={MOCK_ACTIVE}
+    history={mockHistory}
   />);
 
   const movieCard = smallMovieCard.find(`.small-movie-card`);
   movieCard.simulate(`mouseenter`);
   movieCard.simulate(`mouseleave`);
   expect(mockHandler).toHaveBeenCalledTimes(1);
+});
+
+it(`SmallMovieCard handlerClick correctly return redirect path`, () => {
+  const mockFunctionFocus = jest.fn();
+  const smallMovieCard = shallow(<SmallMovieCard
+    movie={TEST_MOCKS.film}
+    onFocus={mockFunctionFocus}
+    onBlur={jest.fn()}
+    active={MOCK_ACTIVE}
+    history={mockHistory}
+  />);
+
+  const movieCard = smallMovieCard.find(`.small-movie-card`);
+  movieCard.simulate(`click`, mockEvt);
+  expect(mockHistory.push).toHaveBeenCalledTimes(1);
+  expect(mockHistory.push).toHaveBeenCalledWith(`/film/:${TEST_MOCKS.film.id}`);
 });
