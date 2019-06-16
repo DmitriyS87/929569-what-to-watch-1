d4225adb09e84969a0de35bdf6bc2329cfd77ceb
DmitriyS87 <schukindmitriy@gmail.com>
2019-06-17 00:59:56 +0800

    makes tests for all current utils functions

--- src/utils/utils.test.tsx
+++ src/utils/utils.test.tsx
@@ -1,15 +1,19 @@
 import { excludeItemById } from './exclude-item-by-id';
+import { getLimitedItems } from './get-limited-items';
+import { getFiltredMovies } from './get-filtred-movies';
+
+import testMock from '../mocks/test-mocks.js';
+
+const mockItems = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+];
 
 it(`excludeItemById correctly deletes first element`, function () {
-  const mockItems = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-  ];
-
   const result = excludeItemById(1, mockItems);
 
   expect(result).toHaveLength(5);
@@ -17,14 +21,6 @@ it(`excludeItemById correctly deletes first element`, function () {
 });
 
 it(`excludeItemById correctly deletes middle element`, function () {
-  const mockItems = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-  ];
   const result = excludeItemById(3, mockItems);
 
   expect(result).toHaveLength(5);
@@ -32,17 +28,44 @@ it(`excludeItemById correctly deletes middle element`, function () {
 });
 
 it(`excludeItemById correctly deletes last element`, function () {
-  const mockItems = [
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { id: 4 },
-    { id: 5 },
-    { id: 6 },
-  ];
   const result = excludeItemById(6, mockItems);
 
   expect(result).toHaveLength(5);
   expect(result).not.toContainEqual([{ id: 6 }]);
+});
+
+it(`getLimitedItems correctly return result with empty array`, function () {
+  const mockItemsZero = [];
+  const result = getLimitedItems(6, mockItemsZero);
+  expect(result).toHaveLength(0);
+  expect(result).toStrictEqual([]);
+});
+
+it(`getLimitedItems correctly return result with limit > items.length`, function () {
+  const result = getLimitedItems(10, mockItems);
+  expect(result).toHaveLength(6);
+  expect(result).toBe(mockItems);
+});
+
+it(`getLimitedItems correctly return result with limit = items.length`, function () {
+  const result = getLimitedItems(6, mockItems);
+  expect(result).toHaveLength(6);
+  expect(result).toBe(mockItems);
+});
+
+it(`getLimitedItems correctly return result with limit < items.length`, function () {
+  const result = getLimitedItems(3, mockItems);
+
+  expect(result).toHaveLength(3);
+});
+
+it(`getFiltredMovies correctly return all movies with genre:defaultGenre param`, function () {
+  const result = getFiltredMovies(`All genres`, testMock.films);
+  expect(result).toHaveLength(3);
+});
+
+it(`getFiltredMovies correctly return 2 movies with genre:\'Adventure\' param`, function () {
+  const result = getFiltredMovies(`Adventure`, testMock.films);
+  expect(result).toHaveLength(2);
 });
 
